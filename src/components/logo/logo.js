c4f20f0a11cf0cd3bc264585339da9369933da1b
Gorka Molero <gorka.molero@gmail.com>
2019-09-25 13:00:20 +0200

    PNG logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import MEDIA from 'helpers/mediaTemplates'
-import qtzl from './QTZL_logo.svg'
+import qtzl from './QTZL_logo.png'
 
 const LogoItem = styled.img`
   max-width: 5em;
